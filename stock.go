12e3b774b52f4bbe81cb2e7df11373ddeccbb0f2
mahoganyforest2022 <mahoganyforest2022@gmail.com>
2026-06-24 14:15:32 +0900

    Update stock.go

--- stock.go
+++ stock.go
@@ -274,10 +274,10 @@ func sendToPushPlus(
 	}
 
 	content := fmt.Sprintf(
-		"%s\n"+
-			"代码：%s\n"+
-			"当前价格：%.2f 元\n"+
-			"目标价格：%.2f 元\n\n",
+		"%s \n"+
+			"代码:%s \n"+
+			"当前价:%.2f \n"+
+			"目标价:%.2f \n\n",
 		name,
 		code,
 		current,
@@ -286,7 +286,7 @@ func sendToPushPlus(
 
 	msg := PushPlusMessage{
 		Token:    token,
-		Title:    "github股票价格提醒!!",
+		Title:    "父子山股价提醒!!",
 		Content:  content,
 		Template: "txt",
 	}
